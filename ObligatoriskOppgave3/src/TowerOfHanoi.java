8af0ef05eca791b4b7757d49239e65a98900df5a
531003@student.hin.no <Marianne>
2015-03-09 09:29:45 +0100

    TowerOfHanoi v0.2

---
+++ ObligatoriskOppgave3/src/TowerOfHanoi.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+
+	private static Scanner input = new Scanner(System.in);
+	private static long cunt = 1;
+	private static long callCunt = 1;
+
+	public static void main(String[] args) {
+		System.out.print("Skriv inn antall skiver: ");
+		int n = input.nextInt();
+
+		System.out.println("Trekkene er:");
+		moveDicks(n, 'A', 'B', 'C');
+
+		System.out.printf("\nAntall tegn ble: %5d\n", cunt);
+	}
+
+	public static void moveDicks(int n, char fromTower, char toTower,
+			char auxTower) {
+		if (n == 1) {
+			System.out.printf(
+					"Flytt skiven %2d fra %c til %c\n", cunt,
+					n, fromTower, toTower);
+			cunt++;
+		} else {
+			moveDicks(n - 1, fromTower, auxTower, toTower);
+			System.out.printf(
+					"Flytt skiven %2d fra %c til %c\n", cunt,
+					n, fromTower, toTower);
+			cunt++;
+			moveDicks(n - 1, auxTower, toTower, fromTower);
+		}
+	}
+}
+
